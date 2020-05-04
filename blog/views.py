7f08d9644db757a3b1dcfa9746621f68ccbc96bb
derzeitgeistmmm <kaldehjerte@gmail.com>
2020-05-04 18:12:53 +0300

    Authentification, drafts and comments are added.

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .form import PostForm
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 
 
@@ -12,18 +13,25 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts_set})
 
 
+@login_required
+def post_drafts_list(request):
+    posts_set = Post.objects.filter(published_date__isnull=True).\
+        order_by('created_date')
+    return render(request, 'blog/post_drafts_list.html', {'posts': posts_set})
+
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -31,6 +39,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -42,3 +51,21 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if post.published_date:
+        post.delete()
+        return redirect('post_list')
+    else:
+        post.delete()
+        return redirect('post_drafts_list')
